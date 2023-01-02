d9ab8b56468ede3c93e6a3012f48bf011f34aad5
Robin Smekens <robin.smekens.930@gmail.com>
2023-01-02 22:31:42 +0100

    using chrono to display time

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,53 @@
+use chrono::prelude::*;
+
+/// How often we repaint the demo app by default
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum RunMode {
+    /// This is the default for the demo.
+    ///
+    /// If this is selected, Egui is only updated if are input events
+    /// (like mouse movements) or there are some animations in the GUI.
+    ///
+    /// Reactive mode saves CPU.
+    ///
+    /// The downside is that the UI can become out-of-date if something it is supposed to monitor changes.
+    /// For instance, a GUI for a thermostat need to repaint each time the temperature changes.
+    /// To ensure the UI is up to date you need to call `egui::Context::request_repaint()` each
+    /// time such an event happens. You can also chose to call `request_repaint()` once every second
+    /// or after every single frame - this is called `Continuous` mode,
+    /// and for games and interactive tools that need repainting every frame anyway, this should be the default.
+    Reactive,
+
+    /// This will call `egui::Context::request_repaint()` at the end of each frame
+    /// to request the backend to repaint as soon as possible.
+    ///
+    /// On most platforms this will mean that Egui will run at the display refresh rate of e.g. 60 Hz.
+    ///
+    /// For this demo it is not any reason to do so except to
+    /// demonstrate how quickly Egui runs.
+    ///
+    /// For games or other interactive apps, this is probably what you want to do.
+    /// It will guarantee that Egui is always up-to-date.
+    Continuous,
+}
+
+/// Default for demo is Reactive since
+/// 1) We want to use minimal CPU
+/// 2) There are no external events that could invalidate the UI
+///    so there are no events to miss.
+impl Default for RunMode {
+    fn default() -> Self {
+        RunMode::Reactive
+    }
+}
+
+#[derive(Default)]
+pub struct TimeData {
+    time_hours: i32,
+    time_minutes: i32,
+    time_seconds: i32,
+}
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
@@ -8,6 +58,21 @@ pub struct TemplateApp {
     // this how you opt-out of serialization of a member
     #[serde(skip)]
     value: f32,
+
+    #[serde(skip)]
+    run_mode: RunMode,
+
+    #[serde(skip)]
+    current_date_time : TimeData,
+
+    #[serde(skip)]
+    pomodoro_time : TimeData,
+
+    #[serde(skip)]
+    rest_time : TimeData,
+
+    #[serde(skip)]
+    runtime_timer : TimeData,
 }
 
 impl Default for TemplateApp {
@@ -16,6 +81,11 @@ impl Default for TemplateApp {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            run_mode: RunMode::Continuous,
+            current_date_time: TimeData::default(),
+            pomodoro_time: TimeData::default(),
+            rest_time: TimeData::default(),
+            runtime_timer: TimeData::default()
         }
     }
 }
@@ -45,13 +115,15 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let label = &mut self.label;
+        let value = &mut self.value;
+        let run_mode = self.run_mode;
+        let time_data = &mut self.current_date_time;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
         // Tip: a good default choice is to just keep the `CentralPanel`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
-
         #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
@@ -76,6 +148,12 @@ impl eframe::App for TemplateApp {
             if ui.button("Increment").clicked() {
                 *value += 1.0;
             }
+
+            let local_time : DateTime<Local> = Local::now();
+            time_data.time_hours = local_time.hour() as i32;
+            time_data.time_minutes = local_time.minute() as i32;
+            time_data.time_seconds = local_time.second() as i32; 
+            ui.label(format!("{}:{}:{}", time_data.time_hours, time_data.time_minutes, time_data.time_seconds));
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
@@ -112,5 +190,12 @@ impl eframe::App for TemplateApp {
                 ui.label("You would normally choose either panels OR windows.");
             });
         }
+
+        if run_mode == RunMode::Continuous {
+            // Tell the backend to repaint as soon as possible
+            ctx.request_repaint();
+        }
     }
+
+    
 }
