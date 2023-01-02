98607791a3ebc98cfd03ad8f17fe3df10d8aac98
Robin Smekens <robin.smekens.930@gmail.com>
2023-01-02 21:46:53 +0100

    adjusted template to new name

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "rusty_pomodoro",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(rusty_pomodoro::TemplateApp::new(cc))),
     );
 }
 
